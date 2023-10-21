d916f64fac60c91491dc4d9ddbaf08cf439dc395
Coding With Cameron <137846994+cwcinc@users.noreply.github.com>
2023-10-20 18:55:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function displayLB() {
-  fetch();
+function viewsLB() {
+  getViews("LM8lPNddwB8");
+}
+
+function getViews(videoID) {
+  //F = fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoID}&key=${api_key}`).then((content) => {console.log(content)});
+  F = fetch("https://www.youtube.com/watch?v=whUTOvqZHy4").then((content) => {console.log(content)});
+  console.log(F);
+  
+  return F;
 }
