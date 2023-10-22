2c86d92e778b0b487d74adb5a09d45c22c69a2f7
Coding With Cameron <137846994+cwcinc@users.noreply.github.com>
2023-10-21 19:26:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const colorize = {
+  "UChI0J7F5WGGM8MoPJbeOn3A":["#042a19", "Itl' TJ music"],
+  "UCKTClHQ95gmeSDbjV8d42hg":["#126615", "The Kid From Brooklyn Music"],
+  "UCzwh7qYbaZJZSSYHHrfhWZg":["#E69696", "Crescendo With Cameron"],
+  "UCONQdshorUnIuwvaC6fw6Yw":["#FE6900", "LifeIsGood"],
+  "UCnCQmFq11fNZEte0OdHUYDw":["#ffca53", "Cash Money"],
+  "UCR9xj3lNRYohpcw6UbYMACg":["#000033", "Keu Music"],
+  "UCjftBOYgvHPH-sY8O_fUfmA":["#451b0a", "LEROY the PANTRY BOY"],
+  "UCSQivt4Ik5V8cg7q6fAveUw":["#4d3522", "Al Pharoh Music"],
+  "UC7ko6OXvlWRXIr9OCt-T46w":["#2a6955", "potatojuiceultra"]
+}
+
+
 function viewsLB() {
   getPlaylist().then(IDs => {
     var promises = [];
@@ -12,7 +25,7 @@ function viewsLB() {
     var sorted = [...outArr].sort((a, b) => b[1] - a[1]);
     var mainList = [];
     for (let i = 0; i < sorted.length; i++) {
-      mainList.push([`${sorted[i][0]}  --  ${sorted[i][1]} views`, sorted[i][2]]);
+      mainList.push([`${sorted[i][0]}  --  ${sorted[i][1]} views`, sorted[i][2], sorted[i][3]]);
     }
     addLB(mainList);
   });
@@ -44,8 +57,16 @@ function getViews(videoID) {
     nameIndex = data.indexOf('"title":"');
     nameEnd = data.indexOf('","lengthSeconds');
     vidName = data.slice(nameIndex + 9, nameEnd);
-    
-    return [vidName, viewCount, videoID];
+
+    creatorIndex = data.indexOf('","channelId":"') + 15;
+    creatorEnd = creatorIndex + 24
+    creatorID = data.slice(creatorIndex, creatorEnd);
+    if (!(creatorID in colorize)) {
+      console.log(videoID);
+      console.log(creatorID);
+      console.log(data);
+    }
+    return [vidName, viewCount, videoID, creatorID];
   });
   
   return F;
@@ -53,10 +74,24 @@ function getViews(videoID) {
 
 function addLB(lbArr) {
   var LB = document.getElementById("LB");
+  LB.innerHTML = "";
   
   for (i = 0; i < lbArr.length; i++) {
     var LBItem = document.createElement("li");
-    LBItem.innerHTML = `<a href="https://www.youtube.com/watch?v=${lbArr[i][1]}" target="_blank">${lbArr[i][0]}</a>`;
+    console.log(lbArr[i][2]);
+    var color = colorize[lbArr[i][2]]
+    console.log(color);
+    if (typeof color == "undefined") {
+      color = "#AAAAAA";
+    } else {
+      color = color[0];
+    }
+    console.log(color);
+    LBItem.innerHTML = `<a href="https://www.youtube.com/watch?v=${lbArr[i][1]}" target="_blank" style="background: ${color}">${lbArr[i][0]}</a>`;
     LB.appendChild(LBItem);
   }
+}
+
+function TEST() {
+  addLB([["AIP", "wrararawr", "UCR9xj3lNRYohpcw6UbYMACg"], ["Ive got 3er3", "url", "UCzwh7qYbaZJZSSYHHrfhWZg"], ["too soon", "spooky", "UCKTClHQ95gmeSDbjV8d42hg"]])
 }
